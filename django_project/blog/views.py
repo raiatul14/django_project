07c97b0c5fdc52410c0fce1ed4597754e06eb88c
Atul Rai <mrsolo214101998@yahoo.com>
2019-05-18 20:34:01 +0530

    Added Templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+posts = [
+			{
+				'author' : 'Atul Rai',
+				'title' : 'Blog Post',
+				'content' : 'First Post',
+				'date_posted' : '18 May 2019'
+			},
+			{
+				'author' : 'Prime Suspect',
+				'title' : 'Blog Post-1',
+				'content' : 'Second Post',
+				'date_posted' : '19 May 2019'
+			},
+		]
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context={
+		'posts' : posts
+	}
+	return render(request, 'blog/home.html', context)
 def about(request):
-	return HttpResponse('<h1>This about page</h1>')
+	return render(request, 'blog/about.html', {'title':'About'})
