9342b781afe1e2e7d1d9bac304a1692238fe0a6d
Atul Rai <mrsolo214101998@yahoo.com>
2019-05-22 10:41:57 +0530

    Added Profile and Picture

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from users import views as user_views
 urlpatterns = [
@@ -10,3 +12,6 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     path('about/',views.about, name='blog-about')
 ]
+
+if settings.DEBUG:
+	urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
