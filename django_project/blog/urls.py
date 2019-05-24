674c0a8a50ee52fbfa27b1015e950e0507366814
Atul Rai <mrsolo214101998@yahoo.com>
2019-05-24 11:37:01 +0530

    Create, Update and Delete operations on Posts

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 from . import views
+from .views import (
+	PostListView,
+	PostDetailView,
+	PostCreateView,
+	PostDeleteView,
+	PostUpdateView)
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from users import views as user_views
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('register/',user_views.register,name='register'),
     path('profile/',user_views.profile,name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
